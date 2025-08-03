637c6fecfcc7796caacc801e061bc86578ecea14
Steven Kreschollek <steven@kreschollek.com>
2025-08-02 21:53:32 -0500

    Add redirect for now.

---
+++ app/menu/[restaurant]/[menu]/route.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env";
+import { NextRequest } from "next/server";
+
+
+
+
+export async function GET(request: NextRequest, { params }: { params: Promise<{ restaurant: string, menu: string }> }): Promise<Response> {
+    const { restaurant, menu } = await params;
+
+    return Response.redirect(`${env.NEXT_PUBLIC_APP_URL}/menu/${restaurant}/${menu}`);
+  }
